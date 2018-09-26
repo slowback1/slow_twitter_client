90a1425960bac202819bdb6306f06ba25dd18da9
Drew Wobeck <dwobeck@gmail.com>
2018-09-26 23:12:40 +0000

    connected to server, changed about page

--- slow_twitter/src/body.js
+++ slow_twitter/src/body.js
@@ -19,7 +19,7 @@ class Body extends Component {
                     <div className="body">
                         <div className="bodySection">
                             <h3> About This Project </h3>
-                            <p> This webapp takes a given Twitter username, parses their tweets, and tries to create an average sentence out of their words. </p>
+                            <p> This webapp takes a given Twitter username, parses their tweets, and tries to create a word bubble separated by their respective word types (nouns, verbs, adjectives, adverbs, and "others"). </p>
                             <p> This webapp is a MERN stack app built by Andrew Wobeck.  To view more of his work, visit his Github page <a href="https://github.com/slowback1"> here </a>.</p>
                         </div>
                     </div>
